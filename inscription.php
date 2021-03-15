63500d6882edaa1fc22a7caa67d1b6a05b7f98af
hugo Blanc <hblanc001@ensc.fr>
2021-03-15 15:47:23 +0100

    modif inscription

--- inscription.php
+++ inscription.php
@@ -22,18 +22,18 @@ include_once('include/header.php');
           <form class="position-relative text-primary " style ="text-align: center" method="post" action ="inscription.php" enctype="multipart/form-data"><br />
 
           <label for="id"> Pseudo :</label>
-            <input class= "position-relative"  style="left: 79px" name="usrname" type="text" id="id" />
+            <input class= "position-relative"  style="left: 79px" name="usrname" type="text" id="id" placeholder="Entrez votre pseudo"/>
               <br /><br />
 
           <label for="password"> Mot de Passe :</label>
-            <input class= "position-relative"  style="left: 57px" type="password" name="password" id="password" />
+            <input class= "position-relative"  style="left: 57px" type="password" name="password" id="password" placeholder="Entrez votre mot de passe" />
               <br /><br />
 
           <label for="mail"> Mail :</label>
-            <input class= "position-relative"  style="left: 90px"  type="mail" name="mail" id="mail" />
+            <input class= "position-relative"  style="left: 90px"  type="mail" name="mail" id="mail" placeholder="Entrez votre mail"/>
               <br /> <br />
           <label for="promo">Promotion  :</label>
-            <input class= "position-relative"  style="left: 90px"  type="text" name="promo" id="promo" />
+            <input class= "position-relative"  style="left: 90px"  type="text" name="promo" id="promo" placeholder="Entrez votre promo"/>
               <br /> <br />
 
           <button class=" position-relative btn-lg btn-info" style=""  >  S'inscrire</button>
